4d95c969b32afc9c2a33bd0a5fb51e0879d6c03e
ofcparvej <ofcparvej.mo@gmail.com>
2025-07-23 15:19:31 +0530

    Added React-1 hands on

---
+++ Week___6__React/1-ReactJS-HOL/myfirstreact/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+   <h1 style={{ textAlign: "center" }}>
+     Welcome the first session of React
+   </h1>
+  );
+}
+
+export default App;
